8afb7a2fbdba6d07ec5a63eb84f284def8eaf487
Cyoungju <dudwn3528@gmail.com>
2024-04-14 15:57:01 +0900

    first commit - react structure

---
+++ src/pages/NotFoundPage/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NotFonundpage(){
+    return <div>
+        NotFonundpage
+    </div>
+}
+
+export default NotFonundpage
---
+++ src/pages/RegisterPage/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ResigterPage(){
+    return <div>
+        CartPage
+    </div>
+}
+
+export default Cartpage
